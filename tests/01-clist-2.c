90c1981e1c42ac6169051bbf6ecd5cf0b9eef804
Andrew Molis <andrewmolis@gmail.com>
2021-04-04 09:23:57 -0700

    Added simple test for iterate

--- tests/01-clist-2.c
+++ tests/01-clist-2.c
@@ -25,6 +25,23 @@ subtest("Simple test clist_add and clist_get",
     clist_destroy(test_list);
 });
 
+subtest("Simple test for clist_iterate and clist_iterate_rev",
+{
+    int capacity = 5;;
+    struct clist *test_list = clist_create(capacity, sizeof(int));
+    for(int i=0; i<15; i++) {
+        clist_add(test_list, &i);
+    }
+
+    struct clist_iterator iter = clist_create_iter();
+    void *elem;
+    for (int i=14; i>=10; i--) {
+        printf("%d\n", i);
+        elem = clist_iterate(test_list, &iter);
+        int iterate_output = *((int *) elem);
+        test_assert(iterate_output == i);
+    }
+});
 
 
 test_end
